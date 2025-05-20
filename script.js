4b257c490827c27dbac690fe09c0d2a0edfe9a94
Safwan <safwanmv007@gmail.com>
2025-05-20 23:12:00 +0530

    fix: some logical error fixed

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const { createElement } = require("react");
 
 const board = document.getElementById("game-board");
 const restart = document.getElementById("restart-btn")
@@ -11,11 +10,11 @@ let matchedPairs = 0;
 
 const shuffle = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1))
-        [array[i], array[j]] = [array[j], array[i]]
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
-}
+};
 
 const createCard = (number) => {
     const card = document.createElement("div");
@@ -31,27 +30,28 @@ const createCard = (number) => {
     back.classList.add("card-back");
     back.textContent = number;
 
-    inner.appendChild(front);
+    inner.appendChild(front)
     inner.appendChild(back)
     card.appendChild(inner)
 
-    card.addEventListener("click", () => flipCard(card, emoji));
+    card.addEventListener("click", () => flipCard(card, number));
     return card;
 }
 
 function flipCard(card, number) {
-    if (flippedCards === 2 || card.classList.contains("flipped") || card.classList.contains("matched")) {
+    if (flippedCards.length === 2 || card.classList.contains("flipped") || card.classList.contains("matched")) {
         return;
     }
-    card.classList.add("Flipped")
+    card.classList.add("flipped")
     flippedCards.push({ card, number })
 
     if (flippedCards.length === 2) {
         const [first, second] = flippedCards;
+
         if (first.number === second.number) {
             first.card.classList.add("matched");
             second.card.classList.add("matched");
-            flippedCards++;
+            matchedPairs++;
 
             if (matchedPairs === numbers.length) {
                 setTimeout(() => {
@@ -79,12 +79,12 @@ function setupGame(){
     winMessage.style.display='none';
     winMessage.style.animation='none';
 
-    shuffle(cards).forEach((numbers) => {
-        const card= createCard(numbers)
+    shuffle(cards).forEach((num) => {
+        const card= createCard(num)
         board.appendChild(card)
         
     });
 }
 
-restart.addEventListener("click",setupGame)
+restart.addEventListener("click", setupGame)
 setupGame();
